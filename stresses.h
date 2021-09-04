e3bbafb8dbfc50d6cb38d1b2c20f849e0549a2a4
adityaparik <88336367+adityaparik@users.noreply.github.com>
2021-09-03 20:16:00 -0600

    Update stresses.h

--- stresses.h
+++ stresses.h
@@ -20,58 +20,45 @@ double interfacearea_axi (scalar f) {
   return area;
 }
 
+int n_drops=-1;
+
 typedef struct {
   double vol;
   double ke;        // Droplet absolute KE
 //  double rel_ke=0.;
   double s_en;           // Droplet Surface Energy
-  double xmax;
-  double xmin;
-  double ymax;
-  double ymin;
 #if dimension==3
-  double zmax;
-  double zmin;
+  double cmz;
+  double uz;
 #endif
-  coord cm;
-  coord vel;
+  double cmx;
+  double cmy;
+  double ux;
+  double uy;
 } DropStats;
 
-typedef struct {
-  Dropstats *ptr = NULL;
-  Dropstats bulk;
-  int n_drops;
-} Fragments;
-
-DropStats dropstats ( const scalar f, const vector u ) {
-  Fragments drops;
-  scalar m[];
+DropStats * atoms ( const scalar f, const vector u, scalar m ) {
   foreach()
     m[] = f[] > 1e-3;
-  drops.n_drops = tag(m);
-
-  static DropStats atom[n_drops];
-  drops.ptr = atom;
-
+  n_drops = tag(m);
+  
+  if (n_drops==-1)
+    static DropStats atom[n_drops];
+  
 // Initialize the stat variables to zero  
   for (int j = 0; j < n_drops; j++) {
     atom[j].vol;
     atom[j].ke=0.;        // Droplet absolute KE
   //  double rel_ke=0.;
     atom[j].se=0.;           // Droplet Surface Energy
-    atom[j].xmax=0;
-    atom[j].xmin=WIDTH;
-    atom[j].ymax=0;
-    atom[j].ymin=WIDTH;
-  #if dimension==3
-    atom[j].zmax=0;
-    atom[j].zmin=WIDTH;
-    atom[j].cm={0.,0.,0.};
-    atom[j].vel={0.,0.,0.};
-  #else
-    atom[j].cm={0.,0.};
-    atom[j].vel={0.,0.};
-  #endif
+    atom[j].cmx=0.;
+    atom[j].cmy=0.;
+    atom[j].ux=0.;
+    atom[j].uy=0.;
+#if dimension==3
+    atom[j].cmz=0.;
+    atom[j].uz=0.;
+#endif
   }
 
   foreach_leaf()
@@ -79,54 +66,45 @@ DropStats dropstats ( const scalar f, const vector u ) {
       int j = m[] - 1;
       atom[j].vol += dv()*f[];
       coord p = {x,y,z};
-      foreach_dimension()
-        atom[j].cm.x += dv()*f[]*p.x;
-      
-      atom[j].vel.x += u.x[]*dv()*f[];
-      atom[j].vel.y += u.y[]*dv()*f[];
+      atom[j].cmx += dv()*f[]*p.x;
+      atom[j].cmy += dv()*f[]*p.y;
+      atom[j].ux += u.x[]*dv()*f[];
+      atom[j].uy += u.y[]*dv()*f[];
+#if dimension==3
+      atom[j].cmz += dv()*f[]*p.z;
+      atom[j].uz += u.z[]*dv()*f[];
+#endif
+      }
     }
 
   MPI_Allreduce (MPI_IN_PLACE, atom, n_drops, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  
 
   for (int j = 0; j < n_drops; j++) {
-    drops.bulk.cm.x += b[j].x;
-    bulk.cm.y += b[j].y;
-    bulk.cm.z += b[j].z;
-    bulk.vol += vol[j];
+    atom[j].cm.x = atom[j].cm.x/atom[j].vol;
+    atom[j].cm.y = atom[j].cm.y/atom[j].vol;
+    atom[j].u.x = atom[j].cm.x/atom[j].vol;
+    atom[j].u.y = atom[j].cm.y/atom[j].vol;
+#if dimension==3
+    atom[j].cm.z = atom[j].cm.z/atom[j].vol;
+    atom[j].u.z = atom[j].cm.z/atom[j].vol;
+#endif
   }
-
-  bulk_com[0] = bulk_com[0]/bulk_vol;    // Bulk COM for camera location
-  bulk_com[1] = bulk_com[1]/bulk_vol;
-  bulk_com[2] = bulk_com[2]/bulk_vol;
-  
-  double xmin = WIDTH, xmax = 0.;
-  double ymin = WIDTH, ymax = 0.;
-
   foreach( reduction(min:xmin) reduction(max:xmax) reduction(min:ymin) reduction(max:ymax) ) {
-    if( f[]>0.01 ) {
-      if ( fabs(y) < 0.02 ) {
-        if (x>xmax)  xmax = x;
-        if (x<xmin)  xmin = x;
-      }
-      if (y>ymax)  ymax = y;
-      if (y<ymin)  ymin = y;
+    if (m[] > 0) {
+      int j = m[] - 1;
+      if (x>atom[j].xmax)  atom[j].xmax = x;
+      if (x<atom[j].xmin)  atom[j].xmin = x;
+      if (y>atom[j].ymax)  atom[j].ymax = y;
+      if (y<atom[j].ymin)  atom[j].ymin = y;
+#if dimension==3
+      if (z>atom[j].zmax)  atom[j].zmax = z;
+      if (z<atom[j].zmin)  atom[j].zmin = z;
+#endif
     }
   }
-
-  if (i==0)
-    fprintf ( fout, "iteration_nos\ttime\tdroplet_index\tvolume\tx_loc\ty_loc\tz_loc\tx_vel\ty_vel\txy_AR\n" );
-  
-  int j = 0;
-  
-  fprintf ( fout, "%.8d %.8f %.8d %.8f %.8f %.8f %.8f %.8f %.8f %.8f\n",
-            i, t, j, vol[j], b[j].x/vol[j], b[j].y/vol[j], b[j].z/vol[j], velx[j]/vol[j], vely[j]/vol[j], 0.5*(xmax-xmin)/(ymax-ymin) );
-
-  for (j = 1; j < n_drops; j++)
-       fprintf ( fout, "%.8d %.8f %.8d %.8f %.8f %.8f %.8f %.8f %.8f\n",
-              i, t, j, vol[j], b[j].x/vol[j], b[j].y/vol[j], b[j].z/vol[j], velx[j]/vol[j], vely[j]/vol[j] );
-  
-  fflush (fout);
 }
+
     
         
 
