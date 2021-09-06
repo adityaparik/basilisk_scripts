fde96665648fd5968ca60582b6a98dc33d321015
adityaparik <88336367+adityaparik@users.noreply.github.com>
2021-09-05 21:02:07 -0600

    A sample Basilisk C file to post process Dump

---
+++ dropstats.c
@@ -0,0 +1,99 @@
+double sigma = 0.01;
+double rho1 = 1000;
+double rho2 = 1;
+double mu1 = 50;
+double mu2 = 1;
+#define WIDTH 16
+#define dimension 2
+
+#include "view.h"
+#include "axi.h"
+#include "custom.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+
+scalar f[];
+scalar p[];
+scalar omega[];
+vector g[];
+vector u[];
+// scalar umag[];
+
+char dumpname[80];
+
+int main (int argc, char * argv[]) {
+  if (argc>1) sprintf (dumpname, "%s", argv[1]);
+
+  FILE * fp = fopen ( "dropstats.txt", "a");
+
+  if (restore (file = dumpname)) {
+    fprintf (fp, "Dumpfile restored: %s\n", dumpname);
+
+    if (is_constant(cm)) {
+      scalar * l = list_copy (all);
+      cm = new scalar;
+      free (all);
+      all = list_concat ({cm}, l);
+      free (l);
+    }
+    scalar cmv = cm;
+    foreach()
+      cmv[] = y;
+    cm[top] = dirichlet(y);
+    cm[bottom] = dirichlet(y);
+    
+    foreach()
+      m[] = f[]>1e-3;
+    n_drops = tag(m);
+    printf ("%d\n", n_drops);
+    DropStats atom[n_drops+1];
+    atoms (f, u, atom);
+
+    tensor D[], vs[];
+    vector Tr[];
+
+    deformation_tensor (u, D);
+    viscous_stress (D, vs);
+    coord F_drop = surfaceforces ( f, vs, Tr);
+
+    for (int j = 0; j < n_drops+1; j++)
+      if (pid()==0)
+        fprintf ( fp, "%.8d %.8f %.8f %.8f %.8f %.8f %.8f %.8f\n",
+                  j, atom[j].vol, atom[j].com.x, atom[j].u.x, atom[j].KE,
+                  atom[j].SE, atom[j].l_max.x, atom[j].l_min.x );
+
+    fprintf (fout, "%.8f %.8f\n", F_drop.x, F_drop.y);
+
+    char dirname[80];
+    sprintf (dirname, "./images");
+    mkdir( dirname, 0777);
+    
+    char label[80];
+  
+    clear();
+    view(fov=3,camera="front",tx=-bulk_com/WIDTH, ty=-0.05 );
+    box();
+    draw_vof("f");
+    sprintf (label, "%s/f_%s.ppm", dirname, dumpname);
+    save (label);
+
+    clear();
+    view(fov=3,camera="front",tx=-bulk_com/WIDTH, ty=-0.05 );
+    box();
+    squares( "omega", min=-30, max=30, linear=true, map=cool_warm );
+    draw_vof("f");
+    sprintf (label, "%s/omega_%s.ppm", dirname, dumpname);
+    save (label);
+
+    clear();
+    view(fov=3,camera="front",tx=-bulk_com/WIDTH, ty=-0.05 );
+    box();
+    squares( "l", min=-30, max=30, linear=true, map=cool_warm );
+    draw_vof("f");
+    sprintf (label, "%s/omega_%s.ppm", dirname, dumpname);
+    save (label);
+  }
+
+  else fprintf (ferr, "Dumpfile \"%s\" does not exist!!\n", dumpname );
+  fclose(fp);
+}
